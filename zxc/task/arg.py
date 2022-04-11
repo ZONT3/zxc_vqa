4075d3989f5962f7b352d148d459c6c83ebb0528
ZONT3 <danya10001@gmail.com>
2022-04-11 14:07:47 +0300

    Initial

--- zxc/task/arg.py
+++ zxc/task/arg.py
@@ -6,10 +6,10 @@ p.add_argument('--train', action='store_true', help="Do train")
 p.add_argument('--test', action='store_true', help="Do test")
 p.add_argument('--val', action='store_true', help="Do val")
 
-p.add_argument('--ds_feats', type=str, default=None,
-               help='Dataset image features file [.pt] (ignores ds_img_dir if set)')
-p.add_argument('--ds_img_dir', type=str, default=None, help='Dataset raw images dir [*.png]')
-p.add_argument('--ds_text', type=str, default=None, help='Dataset text data [.json]')
+p.add_argument('--ds-feats', type=str, default=None,
+               help='Dataset image features file [.pt] (ignores ds-img-dir if set)')
+p.add_argument('--ds-img-dir', type=str, default=None, help='Dataset raw images dir [*.png]')
+p.add_argument('--ds-text', type=str, default=None, help='Dataset text data [.json]')
 
 
 def parse_args(src=None):
